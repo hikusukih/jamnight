29a566f982d3842cb9c1fd130db9615fc79a4007
hikusukih <sir.hikus@gmail.com>
2016-11-18 21:31:12 -0500

    Add a lannding page and set it as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'landing/index'
+
   resources :events
   resources :users
   resources :events
-  root 'todos#index'
+
+  root 'landing#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
